327b2fa4597b55cf577bfc57c887a44aa28ede3f
martinristow <martinristov084@gmail.com>
2024-07-06 22:36:07 +0200

    Add input/output functions for processing data
    Implement functions to read input from the console and display output

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print("Test")
-print("zdravo")
+print("Hello World!") #output of this will be Hello World!
+
+print('She said: "Hello" and then left.') #output of this will be She said: "Hello" and then left
+
+print("She said: \"Hello\" and then left.")
+
+print("Hello World!\nHello World!") # \n -> this text will be in new line
+
+print("Hello " + "Martin") #concatenation
+
+print("Hello " + input("What is your name ?"))
+
+#If we want to comment a whole line of code once, then we will press it ctrl + /
+# input("What is your name ?")
+
+print("Enter a first number")
+num1 = int(input())
+print("Enter a second number")
+num2 = int(input())
+print(num1 * num2)
+
+print("Enter a name")
+numOfChar = len(input())
+print(numOfChar)

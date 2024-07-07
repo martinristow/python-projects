ec9281717b3d7ee4cbd81c8a3a9206616c80cbb9
martinristow <martinristov084@gmail.com>
2024-07-07 23:34:18 +0200

    Add BMI calculation program and demonstrate PEMDAS priority
    - Add comments explaining the priority of mathematical operations (PEMDAS).
    - Implement a program to calculate Body Mass Index (BMI) using user input for height and weight:
      - Prompt user for height in centimeters and convert to meters.
      - Prompt user for weight in kilograms.
      - Convert height to float and weight to integer for accurate calculations.
      - Calculate BMI using the formula kg/m^2.
      - Convert the resulting BMI to an integer and print the result.

--- python_syntax/mathematical_operations.py
+++ python_syntax/mathematical_operations.py
@@ -1,1 +1,23 @@
-print("test")
+# priority of execution of mathematical operations
+# PEMDAS :
+# Parentheses ()
+# Exponents **
+# Multiplication *  Division /
+# Addition +  Subtraction -
+
+# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+# The formula is BMI = kg / m^2
+
+# 1st input: enter height in meters e.g: 1.65
+height = input("What is your height in cm? ")
+
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input("What is your weight in kg? ")
+
+
+new_height = float(height)
+new_weight = int(weight)
+
+BMI = (new_weight / (new_height ** 2))
+bmi_new = int(BMI)
+print(bmi_new)

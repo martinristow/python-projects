8b4315eaedcad5420421a52e46d7c2905a807117
martinristow <martinristov084@gmail.com>
2024-07-07 20:16:36 +0200

    Add examples of data types in Python
    - Add string examples, including printing specific characters and concatenation.
    - Add integer examples, including printing and addition of numbers.
    - Add large number examples with underscores and commas for readability.
    - Add a float example to demonstrate decimal numbers.
    - Add boolean examples to show True and False values.

---
+++ python_syntax/data_types.py
@@ -0,0 +1,22 @@
+# Data Types
+
+# String
+print("Hello"[0]) # printing a first character of this word
+print("Hello"[4]) # printing a last character of this word
+print("123" + "456") # concatenate
+print("Hello " + "Martin " + "Ristov!")
+
+# Integer
+print(123) # printing a number
+print(456 + 123) # addition of two numbers
+
+# Large Number
+print(123_456_789) # result will be 123456789
+print(987,654,321) # result will be 987 654 321
+
+# Float
+print(3.14159) # print a number with dot
+
+# Boolean -> only two results
+print(True)
+print(False)

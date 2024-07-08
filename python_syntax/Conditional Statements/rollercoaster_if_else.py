55c23cab406c59871dda0da41607e9e3fb6138e3
martinristow <martinristov084@gmail.com>
2024-07-08 21:43:45 +0200

    Enhance rollercoaster program with photo option and dynamic billing
    - Add initial welcome message for the rollercoaster.
    - Prompt user for their height in centimeters.
    - Check if the user's height is greater than 120 cm to allow riding the rollercoaster.
    - If eligible, prompt user for their age and implement age-based ticket pricing:
      - Child tickets (age < 12) are $5.
      - Youth tickets (age 12-18) are $7.
      - Adult tickets (age > 18) are $10.
    - Prompt user if they want a photo, and add $3 to the bill if they do.
    - Print the total bill amount based on the user's selections.
    - Print a message asking the user to grow taller if they do not meet the height requirement.

--- python_syntax/Conditional Statements/rollercoaster_if_else.py
+++ python_syntax/Conditional Statements/rollercoaster_if_else.py
@@ -1,14 +1,25 @@
 print("Welcome to the rollercoaster")
+
+bill = 0
 height = int(input("What is your height in cm ?"))
 
 if height > 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age ?"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $10.")
+        bill = 10
+        print("Adult tickets are $10.")
+
+wants_photo = input("Would you like a photo? Y or N!")
+if wants_photo == "Y":
+    bill +=3
+    print("Thank you!")
+    print(f"Your bill is: ${bill}!")
 else:
     print("Sorry, you have to grow taller before you can ride!")

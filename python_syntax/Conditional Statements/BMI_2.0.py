2facdfe51a2cdacbfbb081b97656f99510b20bab
martinristow <martinristov084@gmail.com>
2024-07-08 18:23:48 +0200

    Enhance BMI calculator with weight categories
    - Prompt user for their height in meters and weight in kilograms.
    - Calculate BMI using the formula weight / height^2.
    - Add conditional statements to categorize BMI results:
      - Underweight for BMI < 18.5
      - Normal weight for BMI < 25
      - Slightly overweight for BMI < 30
      - Obese for BMI < 35
      - Clinically obese for BMI >= 35
    - Print appropriate messages with the calculated BMI and corresponding weight category.

---
+++ python_syntax/Conditional Statements/BMI_2.0.py
@@ -0,0 +1,17 @@
+# Enter your height in meters e.g., 1.55
+height = float(input())
+# Enter your weight in kilograms e.g., 72
+weight = int(input())
+
+BMI = weight / height ** 2
+
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+  print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese.")

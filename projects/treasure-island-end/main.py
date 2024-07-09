c2e9d81c35a19dec43a32c458cde3b15f2e43c91
martinristow <martinristov084@gmail.com>
2024-07-09 19:51:36 +0200

    Add Treasure Island interactive adventure game
    - Welcome players to Treasure Island and outline the mission to find the treasure.
    - Implement a series of choices for the player:
      - First choice at a crossroad: "left" or "right"
      - Second choice at a lake: "wait" for a boat or "swim" across
      - Third choice at an island house with three doors: choose "red," "yellow," or "blue"
    - Provide outcomes based on the player's choices:
      - Winning outcome for choosing the yellow door.
      - Game over scenarios for other choices, such as being eaten by beasts, attacked by a trout, or falling into a hole.

--- projects/treasure-island-end/main.py
+++ projects/treasure-island-end/main.py
@@ -22,4 +22,34 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 
 
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
+choise1 = input('''
+You're at a cross road. Where do you want to go?
+    Type "left" or "right"
+''').lower()
+
+if choise1 == "left":
+    choise2 = input('''You've come to a lake. There is an island in the middle of the lake.
+    Type "wait" to wait for a boat. Type "swim" to swim across.
+    ''').lower()
+    if choise2 == "wait":
+        choise3 = input('''
+        You arrive at the island unharmed. There is a house with 3 doors.
+        One red, one yellow and one blue. Which colour do you choose?
+        ''').lower()
+        if choise3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choise3 == "blue":
+            print("Eaten by beasts.Game Over.")
+        elif choise3 == "red":
+            print("Eaten by beasts.Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over!")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+
+
+else:
+    print("You fell into a hole. Game Over.")

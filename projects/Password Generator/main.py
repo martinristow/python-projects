54569007ffc3914c7e929e8fcc5f518081e6c1c3
martinristow <martinristov084@gmail.com>
2024-07-11 19:44:22 +0200

    Added password generator functionality
    - Implemented user inputs to specify the number of letters, symbols, and numbers.
    - Generated lists of random letters, symbols, and numbers based on user input.
    - Combined the generated lists to form a password.
    - Ensured randomness by shuffling the final password.

---
+++ projects/Password Generator/main.py
@@ -0,0 +1,30 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+letter_lists = []
+for letter in range(nr_letters):
+    letter_lists.append(random.choice(letters))
+
+symbol_lists = []
+for symbol in range(nr_symbols):
+    symbol_lists.append(random.choice(symbols))
+
+number_lists = []
+for number in range(nr_numbers):
+    number_lists.append(random.choice(numbers))
+
+all_letters = [letter_lists, symbol_lists, number_lists]
+
+
+
+
+

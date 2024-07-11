7019e84a75f6e75f5f2480d33658210a33d787e4
martinristow <martinristov084@gmail.com>
2024-07-11 17:19:01 +0200

    Added range-based loops and summation example to the existing fruits iteration code
    - Included a loop to print numbers from 1 to 9.
    - Added another loop to print numbers from 1 to 10, increasing by 3 each time.
    - Implemented a summation loop to calculate the sum of numbers from 1 to 100 using Gauss's formula.

--- python_syntax/Loops and Iteration/For Loops/basics.py
+++ python_syntax/Loops and Iteration/For Loops/basics.py
@@ -4,3 +4,24 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " is very good fruit.")
 print(fruits)
+
+
+print("\n")
+# for loops with range
+for number in range(1,10): # Only numbers 1 to 9 are printed. Increase by 1
+    print(number)
+
+print("\n\n")
+
+# Increase by other number
+for number in range(1, 11, 3): # Increase by 3
+    print(number) # output will be 1, 4, 7 and 10.
+   # Explanation 1, 1 + 3 = 4, 4 + 3 = 7, 7 + 3 = 10
+
+print("\n\n")
+
+# Gauss
+total = 0
+for number in range(1, 101):
+    total += number
+print(total) # Output will be 5050. Matches Gauss's value!

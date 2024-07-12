f87efbadecd9ad4311b835a226a42f0461ae7a22
martinristow <martinristov084@gmail.com>
2024-07-12 13:16:00 +0200

    Add functions to simulate movements and draw a square
    - Define `turn_left` function to simulate turning left.
    - Define `move` function to simulate movement.
    - Define `turn_right` function using three `turn_left` calls.
    - Add sequence of commands to simulate drawing a square.

---
+++ python_syntax/Loops and Iteration/Functions/Reeborg's World/main.py
@@ -0,0 +1,18 @@
+def turn_left():
+    print("Turning left...")
+def move():
+    print("Moving...")
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    print("Turning right...")
+# Draw a Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()

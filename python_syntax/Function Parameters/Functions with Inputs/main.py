dfe8fbbcfbd5707feb5680c3d578894c55cb249c
martinristow <martinristov084@gmail.com>
2024-07-16 14:09:30 +0200

    Add greet_with() function for personalized greeting with multiple parameters
    - Implemented greet_with(name, age, sex, number) function to print a detailed personalized greeting.
    - Added examples demonstrating both positional and keyword arguments for the new function.

--- python_syntax/Function Parameters/Functions with Inputs/main.py
+++ python_syntax/Function Parameters/Functions with Inputs/main.py
@@ -20,3 +20,14 @@ def greet_with_input(name):
     print(f"How are you {name}")
 
 greet_with_input("Martin")
+
+# Functions with more than 1 input
+def greet_with(name, age, sex, number):
+    print(f"Welcome {name}, you are {age} old and your sex is {sex}. For more information call on my number {number}. ")
+
+greet_with("Martin",22,"Male","000-999-999") # positional argument
+greet_with("000-999-999","Male","22","Martin") # positional argument
+print("\n")
+greet_with(name = "Martin",age = 22,sex = "Male",number = "000-999-999") # Keywords argument
+print("\n")
+greet_with(age = 22, sex = "Male", number = "999-999-999", name = "Martin") # Keywords argument

767af79827513f73d1e9fe67f2c63a40d9500b3c
martinristow <martinristov084@gmail.com>
2024-07-16 13:28:06 +0200

    Add greeting functions with and without input parameter
    - Implemented a simple greet() function with three print statements.
    - Added greet_with_input(name) function that takes a name as an argument and prints personalized greeting messages.

---
+++ python_syntax/Function Parameters/Functions with Inputs/main.py
@@ -0,0 +1,22 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# Simple Function
+def greet():
+    print("This is a first statement.")
+    print("This is a second statement.")
+    print("This is a third statement.")
+
+greet()
+
+print("\n")
+
+# Function that allows for input
+# name is a Parameter
+# "Martin" is a Argument
+def greet_with_input(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}")
+
+greet_with_input("Martin")

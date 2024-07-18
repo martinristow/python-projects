454cad9e785ed9e608767210d5572c0d575562a1
martinristow <martinristov084@gmail.com>
2024-07-18 20:59:42 +0200

    Add student grades classification and print functionality
    - Created an empty dictionary called student_grades.
    - Added code to classify student scores into grades and populate the student_grades dictionary.
    - Included logic to assign "Outstanding", "Exceeds Expectations", "Acceptable", or "Fail" based on score thresholds.
    - Added code to print all students with their scores.

---
+++ python_syntax/Data Structures/Dictionaries/Grading program/main.py
@@ -0,0 +1,29 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+    "Martin": 100
+}
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
+
+# TODO-3: Print all students with their scores
+for key in student_scores:
+    print(f"{key} : {student_scores[key]}")

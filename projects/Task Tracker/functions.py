21751b558b980ea1d7de3da2ee1c3919a0b53219
martinristow <martinristov084@gmail.com>
2024-07-23 15:48:27 +0200

    Add filtering and sorting functionalities to Task Tracker
    - Added a function to print all tasks or filter tasks based on status:
      - Users can now view all tasks or filter tasks based on whether they are finished or unfinished.
    
    - Improved the remove_task function:
      - Enhanced functionality to notify users when a task is not found.
    
    - Added a function to sort tasks by priority:
      - Users can now sort and view tasks based on their priority levels (lower, mid, highest).
    
    - Updated the main menu logic to include new functionalities:
      - Users can now choose options to view all tasks, filter tasks based on status, and sort tasks by priority.

--- projects/Task Tracker/functions.py
+++ projects/Task Tracker/functions.py
@@ -19,9 +19,6 @@ def remove_task():
         else:
             print("Task is not find")
 
-def print_all_task():
-    print(tasks)
-
 
 def update_status_task():
     print(f"List od all tasks: {tasks} ")
@@ -32,3 +29,42 @@ def update_status_task():
             task["Status"] = status
             print(f"Successfully updated a status of {task["Name"]}")
 
+
+def print_all_task():
+    finished_task = []
+    unfinished_task = []
+    print("If you want to view all task, enter '1'")
+    print("If you want to view filtered task based on status, enter '2'")
+    number = int(input())
+    if number == 1:
+        print(tasks)
+    elif number == 2:
+        for task in tasks:
+            if task["Status"] == "Finished":
+                finished_task.append(task["Name"])
+            else:
+                unfinished_task.append(task["Name"])
+        print(f"Finished tasks : {finished_task}\n")
+        print(f"Unfinished tasks: {unfinished_task}\n")
+    else:
+        print("You enter a wrong number! Only '1' or '2'.")
+
+#   6) Function to sort tasks:
+# Create a function that sorts tasks by priority and displays the sorted tasks.
+
+def sort_task():
+    lower_task = []
+    mid_task = []
+    highest_task = []
+
+    for task in tasks:
+        if task["Priority"] == "Lower":
+            lower_task.append(task["Name"])
+        elif task["Priority"] == "Mid":
+            mid_task.append(task["Name"])
+        elif task["Priority"] == "Highest":
+            highest_task.append(task["Name"])
+
+    print(f"Lower priority: {lower_task}")
+    print(f"Mid priority: {mid_task}")
+    print(f"Highest priority: {highest_task}")

58a2be995faf265ba2240271287127ca0d7238da
martinristow <martinristov084@gmail.com>
2024-07-23 15:49:16 +0200

    Update main menu logic to include task filtering and sorting
    - Integrated new functions into the main menu:
      - Option '4' allows users to view all tasks or filter tasks based on their status (finished or unfinished).
      - Option '5' allows users to sort tasks by priority (lower, mid, highest) and view the sorted tasks.
    
    - Enhanced user interaction for a more comprehensive task management experience.

--- projects/Task Tracker/main.py
+++ projects/Task Tracker/main.py
@@ -4,9 +4,10 @@ exit_for_app = False
 
 while not exit_for_app:
     print("If you want to add a new task, enter a '1':")
-    print("If you want to remove a task, enter '2':")
-    print("If you want to update status to a task, enter '3':")
-    print("If you want to view all task, enter '4':")
+    print("If you want to remove a task, enter a '2':")
+    print("If you want to update status to a task, enter a '3':")
+    print("If you want to view all task, enter a '4':")
+    print("If you want to sort all task by priority, enter a '5'.")
     task = int(input())
     if task == 1:
         functions.add_task()
@@ -16,5 +17,7 @@ while not exit_for_app:
         functions.update_status_task()
     elif task == 4:
         functions.print_all_task()
+    elif task == 5:
+        functions.sort_task()
     else:
         exit_for_app = True

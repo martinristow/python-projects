a727e1e56a720774f1644a6f86f448ecce8f710e
martinristow <martinristov084@gmail.com>
2024-07-24 23:02:02 +0200

    Integrate show_inventory, update_quantity, and update_price functions
    - Added main loop in `main.py` to handle user interactions and call appropriate functions based on user input.
    - Integrated `show_inventory` function to display all products in the inventory.
    - Integrated `update_quantity` function to update the quantity of existing products in the inventory.
    - Integrated `update_price` function to update the prices of existing products.
    - Added validation checks and error messages for invalid inputs.
    
    This update enhances the functionality of the application by providing options to manage the inventory more efficiently.

--- projects/Inventory and Sales Management for a Small Shop/main.py
+++ projects/Inventory and Sales Management for a Small Shop/main.py
@@ -3,4 +3,20 @@ import functions
 add = True
 
 while add:
-    functions.add_new_product()
+    print("Add a new product press 1")
+    print("Show all products in inventory press 2")
+    print("Do you wanna update currently quantity of some product? Press 3")
+    print("Price update? Press 4")
+    digit = int(input())
+    if digit == 1:
+        functions.add_new_product()
+        # text = input("Do you want to go back? 'yes' or 'no'").lower()
+        # functions.home_page(text)
+    elif digit == 2:
+        functions.show_inventory()
+    elif digit == 3:
+        functions.update_quantity()
+    elif digit == 4:
+        functions.update_price()
+    else:
+        break

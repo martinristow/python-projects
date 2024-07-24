70190538494af0fcff4414dc3c9c356f5c5a33f0
martinristow <martinristov084@gmail.com>
2024-07-24 22:59:21 +0200

    Add functions to show inventory, update quantity, and update price
    - Implemented `show_inventory` function to display all products in the inventory along with their quantities and prices.
    - Added `update_quantity` function to allow users to update the quantity of existing products in the inventory.
    - Created `update_price` function to enable users to update the prices of existing products.
    - Ensured validation for negative quantities and invalid price ranges.
    
    These functions enhance the functionality of the inventory management system, allowing for better control and visibility of product details.

--- projects/Inventory and Sales Management for a Small Shop/functions.py
+++ projects/Inventory and Sales Management for a Small Shop/functions.py
@@ -17,3 +17,44 @@ def add_new_product():
         print("Product already exists in the inventory.")
 
     # print(product_list)
+
+
+def show_inventory():
+    print("List of all products in inventory:")
+    for product in product_list:
+        print(f"Name: {product["Name"]}, Quantity: {product["Quantity"]}, Price: {product["Price"]}")
+
+# def home_page(text):
+#     go_back = False
+#     while not go_back:
+#         if text == "yes":
+#             go_back = True
+#         elif text == "no":
+#             continue
+
+def update_quantity():
+    print("List of all products in inventory with their quantities: ")
+    for product in product_list:
+        print(f"Name: {product["Name"]}, Quantity: {product["Quantity"]}")
+        name = input("Product Name:")
+        if product_exists(name):
+            quantity = int(input("Write a new quantity:"))
+            if quantity < 0:
+                print("You entered a negative number!")
+                continue
+            product["Quantity"] = quantity
+        print("Product not exists in the inventory.")
+
+
+def update_price():
+    print("List of all products in inventory with their prices:")
+    for product in product_list:
+        print(f"Name: {product["Name"]}, Price: {product["Price"]}")
+        name = input("Product Name:")
+        if product_exists(name):
+            new_price = int(input(f"Enter a new price for {product["Name"]}"))
+            if new_price <=0 or new_price >= 100:
+                print("Product prices cannot be $0 or less than $0 and greater than $100.")
+                continue
+            product["Price"] = new_price
+        print("Product not exists in the inventory.")

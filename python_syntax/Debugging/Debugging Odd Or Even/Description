a1709ee94a248b7786ec18424e633fefa317fd73
martinristow <martinristov084@gmail.com>
2024-07-29 19:14:34 +0200

    Correct equality operator in number check logic
    - Fixed the equality operator in the if statement to correctly check if the number is even.
    - Added a prompt to the input function for clarity.

---
+++ python_syntax/Debugging/Debugging Odd Or Even/Description
@@ -0,0 +1,8 @@
+Instructions
+Read this the code in main.py
+Spot the problems 🐞.
+Modify the code to fix the program.
+Fix the code so that it works and passes the tests when you submit.
+
+Hint
+Review the previous lesson and go through the 10 steps to tackle these debugging problems.
---
+++ python_syntax/Debugging/Debugging Odd Or Even/main.py
@@ -0,0 +1,16 @@
+# number = int(input())  # Which number do you want to check?
+#
+# if number % 2 = 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+# This code is fixed
+# Error was in line 3 because had missed one equal (=)
+number = int(input("Which number do you want to check?"))  # Which number do you want to check?
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+

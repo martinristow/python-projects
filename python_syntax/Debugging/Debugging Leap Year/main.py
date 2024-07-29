da6c5f93d8d8b586730850ff3e5bdd1967cda6b5
martinristow <martinristov084@gmail.com>
2024-07-29 19:19:35 +0200

    Correct leap year determination logic
    - Added int() function to convert the input year from string to integer.
    - Ensured the leap year logic correctly determines if a year is a leap year or not.

---
+++ python_syntax/Debugging/Debugging Leap Year/main.py
@@ -0,0 +1,27 @@
+# Which year do you want to check?
+# year = input() # Added int() function for convert string to int number
+#
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+# Which year do you want to check?
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")

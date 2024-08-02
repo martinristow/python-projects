be67f3729c4e7de0d36722bbdb536355bea8c681
martinristow <martinristov084@gmail.com>
2024-08-02 17:07:46 +0200

    Moving
    - Projects that had no oop are push into folder non-oop
    - Projects that had oop are pushing into folder oop

---
+++ python_syntax/OOP/classes/main.py
@@ -0,0 +1,19 @@
+# The creation method of our empty class
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1 # na userot sto go zasleduvame mu go zgolemuvame brojot na sledbenici
+        self.following += 1 # so ova si ja zgolemuvame listata na korisnici koj sto nie gi sledime
+
+# One object of our class :)
+user1 = User(1, "martin")
+
+user2 = User(2, "ristov")
+
+user1.follow(user2)
+print(user2.followers)

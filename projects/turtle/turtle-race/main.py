93e6f44e1bffe9a818231382c86017c3efee291e
martinristow <martinristov084@gmail.com>
2024-08-04 18:43:28 +0200

    Expand turtle race project with race functionality
    - Added the ability to start the race when the user places a bet.
    - Created an `is_race_on` flag to control the race loop.
    - Moved the turtle creation into a list `all_turtles` for easier management.
    - Implemented a while loop to simulate the race, moving each turtle forward by a random distance in each iteration.
    - Determined the race winner by checking which turtle crosses the finish line first.
    - Provided feedback to the user on whether they won or lost the bet based on the winning turtle's color.

--- projects/turtle/turtle-race/main.py
+++ projects/turtle/turtle-race/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
-
+import random
+is_race_on = False
 
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -11,16 +12,35 @@ number_of_turtles = len(colors)
 # y = -100
 # x = -230
 y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle_index in range(0, number_of_turtles):
-    my_turtle = Turtle(shape="turtle")
-    my_turtle.penup()
-    my_turtle.color(colors[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.speed("fastest")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
     # X-axis is along the horizontal , Y-axis is along the vertical
     # (Х-оската е по хоризонталата, Y-оската е долж вертикалата)
     # my_turtle.goto(x=x, y=y)
-    my_turtle.goto(x=-230, y=y_position[turtle_index])
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
     # y += 40
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 240:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()

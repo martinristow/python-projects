3f44998422a6a774c20a7b690714236a99b2e58b
martinristow <martinristov084@gmail.com>
2024-08-04 13:35:31 +0200

    Add basic turtle movement controls with keyboard input
    - Import the Turtle and Screen classes from the turtle module.
    - Create instances of Turtle and Screen.
    - Define functions to move the turtle forwards and backwards:
      - `move_forwards`: Move the turtle forward by 10 units.
      - `move_backward`: Move the turtle backward by 10 units.
    - Set up keyboard listeners:
      - Use `screen.listen()` to start listening for keyboard events.
      - Use `screen.onkey()` to bind the spacebar to the `move_forwards` function.
      - Use `screen.onkey()` to bind the 'a' key to the `move_backward` function.
    - Close the screen with a click using `screen.exitonclick()`.

---
+++ python_syntax/Turtle/Event Listeners/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.forward(-10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="a", fun=move_backward)
+screen.exitonclick()

dc3b413d5aa6c6c792dcbe151d1667a90d0abded
martinristow <martinristov084@gmail.com>
2024-08-05 19:19:15 +0200

    Refactor main.py to use Snake class
    - Updated `main.py` to import and use the `Snake` class from `snake.py`.
    - Created an instance of the `Snake` class.
    - Updated the game loop to call the `move` method of the `Snake` class.
    - Improved code organization by separating the game logic and the snake behavior into different files.

--- projects/oop/snake-game/main.py
+++ projects/oop/snake-game/main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 # Screen Setup
 screen = Screen()
@@ -7,29 +8,13 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-# Creating a Snake Body
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
+    snake.move()
 
 screen.exitonclick()
-

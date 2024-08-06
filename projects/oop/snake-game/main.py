74b8d945169d37b5a9f1e23b7b8710cbf9d61fc5
martinristow <martinristov084@gmail.com>
2024-08-06 20:10:48 +0200

    Add scoreboard functionality to snake game
    - Imported Scoreboard class and instantiated scoreboard object
    - Added collision detection with food to increase score
    - Updated scoreboard when snake eats food

--- projects/oop/snake-game/main.py
+++ projects/oop/snake-game/main.py
@@ -1,7 +1,9 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
+
 # Screen Setup
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,11 +13,12 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
@@ -26,5 +29,6 @@ while game_is_on:
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()

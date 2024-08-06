800a224ef34537564bd28dd06a7aae092de33135
martinristow <martinristov084@gmail.com>
2024-08-06 19:24:21 +0200

    Add food functionality to snake game
    - Imported Food class and instantiated food object
    - Added collision detection between snake and food
    - When the snake collides with food, the food is refreshed to a new random location

--- projects/oop/snake-game/main.py
+++ projects/oop/snake-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 # Screen Setup
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -19,7 +21,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()

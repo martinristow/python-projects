b61f0eb9fceb4cabc38324c521644f347c74e2df
martinristow <martinristov084@gmail.com>
2024-08-06 20:10:27 +0200

    Create Scoreboard class for snake game
    - Defined Scoreboard class inheriting from Turtle
    - Initialized scoreboard with a white color and hid turtle
    - Added methods to update and increase the score

---
+++ projects/oop/snake-game/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 20, "normal")
+MOVE = False
+X_POSITION = 0
+Y_POSITION = 270
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.goto(x=X_POSITION, y=Y_POSITION)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(arg=f"Score: {self.score}", move=MOVE, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()

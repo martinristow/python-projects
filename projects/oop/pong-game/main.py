3faed59d4f004a0b36edf9fa252222ab8cf03ee6
martinristow <martinristov084@gmail.com>
2024-08-08 01:04:38 +0200

    Enhance Pong game with scoring system and improved ball mechanics
    - Added Scoreboard to track and display scores for left and right paddles.
    - Integrated scoring updates when the ball misses paddles.
    - Improved ball speed adjustment and collision detection.
    - Updated game loop to dynamically adjust ball movement speed.

--- projects/oop/pong-game/main.py
+++ projects/oop/pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -38,10 +40,12 @@ while game_is_on:
     # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect L paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()

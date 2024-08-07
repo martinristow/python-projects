0a28ee8e981fc0aa1b33d26abe575f56bf68d774
martinristow <martinristov084@gmail.com>
2024-08-07 15:11:35 +0200

    Add segment extension functionality to snake
    - Implemented a method to add segments to the snake's body.
    - Extended the snake's body when it collides with food.
    - Refactored snake creation to use the new add_segment method.

--- projects/oop/snake-game/snake.py
+++ projects/oop/snake-game/snake.py
@@ -18,11 +18,17 @@ class Snake:
     def create_snake(self):
         # Creating a Snake Body
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def move(self):
 

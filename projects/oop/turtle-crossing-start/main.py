c313173a38dd523bdd7130615b432c43d65561d9
martinristow <martinristov084@gmail.com>
2024-08-08 19:15:52 +0200

    Integrate CarManager into the game loop
    - Initialized CarManager instance in main.py.
    - Updated game loop to create and move cars using CarManager.
    - Ensured continuous car creation and movement for gameplay.

--- projects/oop/turtle-crossing-start/main.py
+++ projects/oop/turtle-crossing-start/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -17,3 +18,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()

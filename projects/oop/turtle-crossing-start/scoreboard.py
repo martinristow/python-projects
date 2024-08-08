16701818b8cfe03a52c68b49211d08c602d7bbdf
martinristow <martinristov084@gmail.com>
2024-08-08 19:53:46 +0200

    Implement scoreboard functionality
    - Created `Scoreboard` class to track and display game levels.
    - Added methods to increase the level and display "GAME OVER" message.
    - Updated `update_scoreboard` method to reflect changes in the level on the screen.

--- projects/oop/turtle-crossing-start/scoreboard.py
+++ projects/oop/turtle-crossing-start/scoreboard.py
@@ -1,6 +1,29 @@
 from turtle import Turtle
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.color("black")
+        self.penup()
+        self.goto(-280, 240)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+
+
+

4640c7fa2bbf0d0af33a27bc068b798a99acbe78
martinristow <martinristov084@gmail.com>
2024-08-08 19:28:38 +0200

    Add level progression and crossing detection
    - Implemented detection for successful crossing of the finish line.
    - Added `level_up` call to increase car speed when the player crosses the finish line.

--- projects/oop/turtle-crossing-start/main.py
+++ projects/oop/turtle-crossing-start/main.py
@@ -27,4 +27,9 @@ while game_is_on:
         if car.distance(player) < 20:
             game_is_on = False
 
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
 screen.exitonclick()

53b05ae1d42a8460ea5e0e8f6c6b2b43542dd37f
martinristow <martinristov084@gmail.com>
2024-08-09 14:26:04 +0200

    Update Scoreboard to track and display high score
    - Implemented high score tracking and display.
    - Added reset method to update high score and reset current score.
    - Removed game over logic for seamless gameplay integration.

--- projects/oop/snake-game/scoreboard.py
+++ projects/oop/snake-game/scoreboard.py
@@ -11,6 +11,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color("white")
         self.hideturtle()
@@ -19,11 +20,17 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", move=MOVE, align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", move=MOVE, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg=f"GAME OVER", move=MOVE, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg=f"GAME OVER", move=MOVE, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1

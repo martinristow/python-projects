5fe526732aa2a9bd033626acb8ef67b24fc7c2a4
martinristow <martinristov084@gmail.com>
2024-08-09 14:26:13 +0200

    Add reset functionality to Snake class
    - Implemented reset method to clear the current snake and create a new one.
    - Updated move method for better collision detection and reset logic integration.

--- projects/oop/snake-game/snake.py
+++ projects/oop/snake-game/snake.py
@@ -30,6 +30,13 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def move(self):
 
         for seg_num in range(len(self.segments) - 1, 0, -1):

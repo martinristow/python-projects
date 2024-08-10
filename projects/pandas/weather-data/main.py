c8b535336825705e516a1781ce06d69e585316af
martinristow <martinristov084@gmail.com>
2024-08-10 14:45:30 +0200

    Refactor main.py to efficiently read temperature data using csv module
    - Replaced manual line-by-line reading with Python's csv module for better efficiency.
    - Implemented a loop to extract and store temperatures from the CSV file.
    - Filtered out the header row and converted temperature values to integers.

--- projects/pandas/weather-data/main.py
+++ projects/pandas/weather-data/main.py
@@ -1,6 +1,20 @@
-with open("weather_data.csv", mode="r") as data_file:
-    data = data_file.readlines()
+# with open("weather_data.csv", mode="r") as data_file:
+#     data = data_file.readlines()
+#     # print(data)
+#     for datat in data:
+#         stripped_data = datat.strip()
+#         print(stripped_data)
+
+# THIS WAY IS SO PAINFUL IF WE HAVE A LOT OF DATA
+import csv
+
+with open("weather_data.csv") as data_file:
+    temperature = []
+    data = csv.reader(data_file)
     # print(data)
-    for datat in data:
-        stripped_data = datat.strip()
-        print(stripped_data)
+    # next(data) # first row is jumped
+    for row in data:
+        if row[1] != "temp":
+            # print(row[1])
+            temperature.append(int(row[1]))
+    print(temperature)

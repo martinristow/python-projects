befbd2674ad6983e01f6d1e40071dbf81798fa25
martinristow <martinristov084@gmail.com>
2024-08-12 13:53:32 +0200

    Generate phonetic code words from user input using NATO phonetic alphabet
    - Created a dictionary from the NATO phonetic alphabet CSV file with letters as keys and code words as values.
    - Implemented functionality to take user input, extract the first letter of each word, and generate a list of corresponding phonetic code words.
    - Ensured the phonetic list only includes valid letters present in the dictionary.

--- projects/pandas/NATO-alphabet-start/main.py
+++ projects/pandas/NATO-alphabet-start/main.py
@@ -1,27 +1,37 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+# student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     print(row)
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
+data_set = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(data_set.code)
+dictionary = {row.letter: row.code for (index, row) in data_set.iterrows()}
+# print(dictionary)
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+text_input = input("Enter a text: ")
+split_text = text_input.split()
+letter_list = [i[0].upper() for i in split_text]
+phonetic_list = [dictionary[letter] for letter in letter_list if letter in dictionary]
+print(phonetic_list)
 

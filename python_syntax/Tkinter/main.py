3b5d5a834bb1f39a6e54c85385641da906f3abdc
martinristow <martinristov084@gmail.com>
2024-08-12 19:26:35 +0200

    Enhance GUI with interactive label and button in tkinter
    - Modified the GUI to update the label text dynamically based on user input.
    - Added an Entry widget to allow users to input text.
    - Created a button with a command to update the label text with the user input from the Entry widget.
    - Utilized config method to change label text and demonstrate dynamic interaction.

--- python_syntax/Tkinter/main.py
+++ python_syntax/Tkinter/main.py
@@ -1,11 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label!", font=("Arial", 15, "normal"))
+my_label = Label(text="I am a Label!", font=("Arial", 15, "normal"))
+# my_label.pack(side="left")
 my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="Hello")
+
+
+def button_clicked():
+    # my_label.config(text="Button Got Clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+# Button
+button = Button(text="Click me", command=button_clicked)
+# button.pack(side="left")
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+
 
 window.mainloop()

495c41f754d50ee3142109cc8da5fdda05088fe7
martinristow <martinristov084@gmail.com>
2024-08-13 13:59:43 +0200

    Enhance Pomodoro Timer UI with Start and Reset Buttons
    - Added a label to display "Timer" at the top of the window for better clarity.
    - Implemented a Start button to initiate the timer countdown.
    - Implemented a Reset button to reset the timer to its initial state.
    - Added a label to show check marks indicating completed work sessions.
    - Organized the UI elements using the grid layout manager for a structured appearance.

--- projects/tkinter/pomodoro-start/main.py
+++ projects/tkinter/pomodoro-start/main.py
@@ -20,12 +20,30 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Image Name
 image = "tomato.png"
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text=r"00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
 
+# Timer Number
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+# Timer Text Label
+timer_text = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+timer_text.grid(column=1, row=0)
+
+# Start Button
+start_button = Button(text="Start", bg="white", width=6, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+# Reset Button
+reset_button = Button(text="Reset", bg="white", width=6, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+# Check Marks Label
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()

6e16d69819ab0bbb0c9dedd3b5cd5e82eb992e26
martinristow <martinristov084@gmail.com>
2024-08-13 15:00:11 +0200

    Implement Timer Countdown Functionality in Pomodoro App
    - Added `start_timer` function to initiate the countdown with the work session duration.
    - Implemented `count_down` function to handle the countdown mechanism, updating the timer display every second.
    - Modified the timer display to format seconds with a leading zero when necessary.
    - Linked the Start button to begin the countdown when clicked.
    - Structured the code to enhance readability and maintainability.

--- projects/tkinter/pomodoro-start/main.py
+++ projects/tkinter/pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,14 +12,28 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(WORK_MIN * 60)
 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 # Image Name
 image = "tomato.png"
@@ -27,15 +42,15 @@ tomato_img = PhotoImage(file=image)
 canvas.create_image(100, 112, image=tomato_img)
 
 # Timer Number
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Timer Text Label
-timer_text = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
-timer_text.grid(column=1, row=0)
+timer_textt = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+timer_textt.grid(column=1, row=0)
 
 # Start Button
-start_button = Button(text="Start", bg="white", width=6, highlightthickness=0)
+start_button = Button(text="Start", bg="white", width=6, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 # Reset Button
@@ -45,5 +60,6 @@ reset_button.grid(column=2, row=2)
 # Check Marks Label
 check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
 
 window.mainloop()

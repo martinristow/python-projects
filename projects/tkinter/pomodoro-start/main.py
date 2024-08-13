d1db7470939f27d16d57c80db5cae517b912d1e8
martinristow <martinristov084@gmail.com>
2024-08-13 15:17:26 +0200

    Implement Work and Break Cycles in Pomodoro App
    - Added a global `reps` variable to track the number of work/break cycles completed.
    - Enhanced `start_timer` function to alternate between work sessions and breaks, updating the UI accordingly.
      - Configured for long breaks every 8th session and short breaks every 2nd session.
      - Updated timer label to display "Work" or "Break" with corresponding color changes.
    - Improved `count_down` function to restart the timer automatically after completion of each cycle.

--- projects/tkinter/pomodoro-start/main.py
+++ projects/tkinter/pomodoro-start/main.py
@@ -9,13 +9,26 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    reps += 1
+    if reps % 8 == 0:
+        timer_textt.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_textt.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_textt.config(text="Work", fg=GREEN)
+        count_down(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -27,9 +40,11 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)

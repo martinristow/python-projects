af5c3aedb6fc323afa29cd45f241aa036a9b48ca
martinristow <martinristov084@gmail.com>
2024-08-14 14:32:45 +0200

    Update UI layout for Password Manager: Added input fields and buttons
    - Added labels and input fields for website, email/username, and password.
    - Implemented 'Generate Password' and 'Add' buttons with appropriate positioning.
    - Adjusted window size and padding for improved layout.
    - Updated canvas to display logo in the center.

--- projects/tkinter/password-manager-start/main.py
+++ projects/tkinter/password-manager-start/main.py
@@ -6,15 +6,46 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
-window.minsize(width=300, height=300)
+window.config(padx=50, pady=50)
+window.minsize(width=350, height=350)
 
 
-# Image Name
-
+# Logo Name
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+# Website Label
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+# Email/Username Label
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(column=0, row=2)
+
+# Password Label
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Website Input
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+# Email/Username Input
+email_username_input = Entry(width=35)
+email_username_input.grid(column=1, row=2, columnspan=2)
+
+# Password Input
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+# Generate Password Button
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+# Add Button
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()

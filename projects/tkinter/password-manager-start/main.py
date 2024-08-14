5662cbd029c025fcf769b256a947bce16af4bc2a
martinristow <martinristov084@gmail.com>
2024-08-14 17:25:04 +0200

    Complete Password Manager project with password generation and saving functionality
    - Implemented a password generator that creates secure passwords with random letters, numbers, and symbols.
    - Integrated pyperclip to automatically copy generated passwords to the clipboard.
    - Updated the UI to include a "Generate Password" button and linked it to the password generator function.
    - Enhanced the save function to store website, email/username, and password details in 'data.txt'.
    - Ensured input fields are validated and confirmation dialog prompts users before saving data.
    - Finalized project UI layout and functionality.

--- projects/tkinter/password-manager-start/main.py
+++ projects/tkinter/password-manager-start/main.py
@@ -1,6 +1,29 @@
 from tkinter import *  # import all classes and constants
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_input.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -66,7 +89,7 @@ password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
 # Generate Password Button
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
 # Add Button

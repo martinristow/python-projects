3ce832faddc34df834fd0ddaf048a9ed3b582eff
martinristow <martinristov084@gmail.com>
2024-08-15 15:12:37 +0200

    Enhance phonetic code generator with KeyError handling and recursion
    - Removed TODO comments as they have been addressed.
    - Implemented a `generate_phonetic` function to encapsulate user input and phonetic code generation.
    - Added exception handling for KeyError to prompt the user to enter valid letters when an invalid character is detected.
    - Used recursion to allow the user to try again after entering invalid input.
    - Ensured that valid phonetic code words are displayed when input is correct.

--- projects/pandas/NATO-alphabet-start/main.py
+++ projects/pandas/NATO-alphabet-start/main.py
@@ -1,10 +1,18 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 data_set = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data_set.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output = [phonetic_dict[letter] for letter in word]
-print(output)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()

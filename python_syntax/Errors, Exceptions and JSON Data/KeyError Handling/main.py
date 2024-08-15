780ca62b4a250c3f43c45ac1a057c784413ce2a1
martinristow <martinristov084@gmail.com>
2024-08-15 14:41:33 +0200

    Add KeyError handling to count_likes function
    - Implemented exception handling for KeyError in the `count_likes` function to avoid program crashes when a post does not have a "Likes" key.
    - Used a try-except block to safely attempt to access the 'Likes' key and add its value to total_likes.
    - Ensured that the absence of the "Likes" key does not cause the program to crash by using 'pass' in the except block.

---
+++ python_syntax/Errors, Exceptions and JSON Data/KeyError Handling/main.py
@@ -0,0 +1,22 @@
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except KeyError:
+            pass
+
+    return total_likes
+
+
+count_likes(facebook_posts)

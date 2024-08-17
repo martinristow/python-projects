53d160e10d87f2cdde0488aed8a2f8b226ed8a51
martinristow <martinristov084@gmail.com>
2024-08-17 17:17:08 +0200

    Learned how to work with dates and times in Python using the datetime module.
     - Added code to print the current day of the week and created a datetime object for a specific date of birth.

---
+++ python_syntax/smtplib-datetime/datetime/main.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=2002, month=3, day=7, hour=12)
+print(date_of_birth)

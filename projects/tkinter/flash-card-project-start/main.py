d86edef52b57c56391336abec620ca6ee07208fc
martinristow <martinristov084@gmail.com>
2024-08-17 14:59:35 +0200

    Final Commit: Completed Flash Card application with persistent learning feature
    - Implemented functionality to save progress by storing unknown words in `words_to_learn.csv`.
    - Added error handling to load data from `french_words.csv` if `words_to_learn.csv` is not found.
    - Enhanced user experience with an "is known" button that removes correctly guessed words and updates the data file.
    - Finalized the flip card functionality to display the translation after a delay.
    - Optimized the user interface with responsive buttons and a clean design.

--- projects/tkinter/flash-card-project-start/main.py
+++ projects/tkinter/flash-card-project-start/main.py
@@ -2,10 +2,15 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -22,6 +27,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -44,7 +56,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()

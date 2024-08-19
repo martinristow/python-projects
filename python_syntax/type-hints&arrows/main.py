0825d0e23e52809284f6a4ec001e6288438a8058
martinristow <martinristov084@gmail.com>
2024-08-19 19:42:24 +0200

    Learned Type Hints in Python
    - Added type hints to variables and function definitions to specify  expected data types.
    - Demonstrated type hinting with variables (age, name, height,       is_human) and a function police_check that checks if a person can  drive.
    - The police_check function now has type hints for the input         parameter and the return type, improving code clarity and type     safety.

---
+++ python_syntax/type-hints&arrows/main.py
@@ -0,0 +1,18 @@
+age: int
+name: str
+height: float
+is_human = bool
+
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+if police_check(21):
+    print("You may pass")
+else:
+    print("Pay a fine")

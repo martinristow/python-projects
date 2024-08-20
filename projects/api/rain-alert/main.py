cf8d45c13754e9b04b0808b90b34444f93da91cc
martinristow <martinristov084@gmail.com>
2024-08-21 00:12:39 +0200

    Implemented weather condition check for Rain Alert project
    - Updated the Rain Alert project to include a check for rain conditions.
    Added a parameter to limit the forecast data to 4 time intervals.
    - Implemented a one-liner list comprehension to extract weather condition codes from the forecast data.
    - Added a loop to check if any of the condition codes indicate rain (code < 700) and print a reminder to bring an umbrella.
    - Commented out the longer, more readable version of the rain check for reference.

--- projects/api/rain-alert/main.py
+++ projects/api/rain-alert/main.py
@@ -3,19 +3,36 @@ import requests
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = ""
 
-MY_LAT = 41.865021
-MY_LONG = 21.943331
+MY_LAT = 49.567550
+MY_LONG = 5.532180
 weather_parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
+    "cnt": 4,
 }
 
 
 response = requests.get(url=OWM_Endpoint, params=weather_parameters)
-print(response.status_code)
+# print(response.status_code)
 
 response.raise_for_status()
 
-data = response.json()
-print(data)
+weather_data = response.json()
+# print(weather_data)
+
+condition_code = [hour_data["weather"][0]["id"] for hour_data in weather_data["list"]]  # One line code for same situation like bellow
+
+# will_rain = False
+# for hour_data in weather_data["list"]: # This is the longer way but code is easier to reading
+#     condition_code = hour_data["weather"][0]["id"]
+#     if int(condition_code) < 700:
+#         will_rain = True
+#
+# if will_rain:
+#     print("Bring un umbrella.")
+
+# print(condition_code)
+for code in condition_code:
+    if int(code) < 700:
+        print("Bring an Umbrella!")

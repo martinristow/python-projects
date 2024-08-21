ecac14004fad7f78bf9574fc238a8a24038c370d
martinristow <martinristov084@gmail.com>
2024-08-21 13:42:02 +0200

    Update Rain Alert project
    - Integrated Twilio API for SMS notifications.
    - Added logic to check weather conditions for rain.
    - Sends an SMS alert if rain is predicted.

--- projects/api/rain-alert/main.py
+++ projects/api/rain-alert/main.py
@@ -1,10 +1,14 @@
 import requests
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = ""
+account_sid = ""
+auth_token = ""
 
-MY_LAT = 49.567550
-MY_LONG = 5.532180
+MY_LAT = 50.264893
+MY_LONG = 19.023781
+
 weather_parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
@@ -23,7 +27,7 @@ weather_data = response.json()
 
 condition_code = [hour_data["weather"][0]["id"] for hour_data in weather_data["list"]]  # One line code for same situation like bellow
 
-# will_rain = False
+will_rain = False
 # for hour_data in weather_data["list"]: # This is the longer way but code is easier to reading
 #     condition_code = hour_data["weather"][0]["id"]
 #     if int(condition_code) < 700:
@@ -35,4 +39,14 @@ condition_code = [hour_data["weather"][0]["id"] for hour_data in weather_data["l
 # print(condition_code)
 for code in condition_code:
     if int(code) < 700:
-        print("Bring an Umbrella!")
+        will_rain = True
+
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring un Umbrella.",
+        from_="",
+        to=""
+    )
+    print(message.status)

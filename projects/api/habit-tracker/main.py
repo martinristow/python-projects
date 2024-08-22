0997f1859fb2e45f765e7379e77398065210c3df
martinristow <martinristov084@gmail.com>
2024-08-22 19:27:26 +0200

    Add graph creation feature to Habit-Tracker project
    - Commented out the initial user creation code.
    - Added code to create a new graph on Pixela with a POST request.
    - Configured the graph with parameters like ID, name, unit, type, and color.
    - Implemented headers to include the user token for authentication.
    - Printed response to confirm successful graph creation.

--- projects/api/habit-tracker/main.py
+++ projects/api/habit-tracker/main.py
@@ -12,5 +12,22 @@ user_params = {
     "notMinor": "yes",
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": " Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
 print(response.text)

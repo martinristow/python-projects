815364a2c4edc32f172352e11a71543b5c645c80
martinristow <martinristov084@gmail.com>
2024-08-22 15:54:28 +0200

    Implemented stock price comparison and percentage calculation
    - Added API request to Alpha Vantage for daily stock prices.
    - Implemented logic to retrieve and compare closing prices for yesterday and the day before.
    - Calculated the percentage difference between the two closing prices.
    - Added condition to trigger news retrieval if the percentage difference exceeds 5%.

--- projects/api/stock-trading-news-alert/stock-news-extrahard-start/main.py
+++ projects/api/stock-trading-news-alert/stock-news-extrahard-start/main.py
@@ -1,29 +1,53 @@
 import requests
-from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-endpoint = ""
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 api_key = ""
 account_sid = ""
 auth_token = ""
 
-parameters = {}
 
-response = requests.get(url="")
+##  STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News"). DONE
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": api_key,
+}
+response = requests.get(url=STOCK_ENDPOINT, params=stock_params)
 print(response)
 response.raise_for_status()
 
-data = response.json()
-print(data)
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+data = response.json()["Time Series (Daily)"]
+#This dict is convert into a list
+data_list = [value for (key, value) in data.items()]
+
+#Get yesterday's closing stock price
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+#Get the day before yesterday's closing stock price
+day_before_yesterday = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday["4. close"]
+print(day_before_yesterday)
+
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+#Abs function return positive number. If the result is -5, after abs function we have +5
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+if diff_percent > 5:
+    print("Get News")
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
 #Optional: Format the SMS message like this: 
@@ -36,4 +60,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-

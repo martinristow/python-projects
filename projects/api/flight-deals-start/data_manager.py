cdf8439675da1ae98cd94c0bb35968603d981068
martinristow <martinristov084@gmail.com>
2024-08-24 23:51:45 +0200

    Add Sheety API integration for managing flight destination data
    - Implemented functionality to fetch destination data from the Google Sheet using Sheety API.
    - Added method to update destination IATA codes in the Google Sheet.
    - Used requests library for HTTP requests.

---
+++ projects/api/flight-deals-start/data_manager.py
@@ -0,0 +1,36 @@
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/103066c320884ef5f0776432af28dee6/flightDeals/prices"
+
+
+class DataManager:
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)

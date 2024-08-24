f4aa448e4233bc0128699c7d668e81c66348b94e
martinristow <martinristov084@gmail.com>
2024-08-24 15:56:19 +0200

    Removed

--- projects/api/flight-deals-start/flight_search.py
+++
@@ -1,6 +0,0 @@
-class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
-
-    def get_iata_code(self, city_name):
-        # For now, just return "TESTING" instead of a real IATA code.
-        return "TESTING"

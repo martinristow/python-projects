4b624fc05f721f97ddeb6709a300a5b5015c04d3
martinristow <martinristov084@gmail.com>
2024-08-24 23:52:39 +0200

    Complete flight deals notifier with full integration of APIs and notifications
    - Integrated data manager, flight search, and notification modules.
    - Implemented logic to update Google Sheets with IATA codes.
    - Added functionality to search for the cheapest flights and send alerts via WhatsApp.
    - Configured the main script to run the complete workflow.

---
+++ projects/api/flight-deals-start/main.py
@@ -0,0 +1,55 @@
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
+
+# ==================== Set up the Flight Search ====================
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+# ==================== Search for Flights and Send Notifications ====================
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination}")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )

ef57b485e7c0cb15c17c1e6cd60630548aa14990
martinristow <martinristov084@gmail.com>
2024-08-28 01:35:43 +0200

    Add custom headers to request for Amazon price tracking
    - Added full and minimal header options to the request for fetching Amazon product data.
    - Improved the reliability of the request by mimicking a real browser using User-Agent and other header fields.
    - Ensured the request is less likely to be blocked or redirected by Amazon.

--- projects/web-scraping/amazon-price-tracker-project/main.py
+++ projects/web-scraping/amazon-price-tracker-project/main.py
@@ -9,7 +9,32 @@ load_dotenv()
 URL = "https://appbrewery.github.io/instant_pot/"
 live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
-response = requests.get(url=URL)
+
+# ====================== Add Headers to the Request ===========================
+
+# Full headers would look something like this
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
+
+# A minimal header would look like this:
+# header = {
+#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+#     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+# }
+
+response = requests.get(url=URL, headers=header)
 amazon_website_html = response.text
 
 soup = BeautifulSoup(amazon_website_html, "html.parser")

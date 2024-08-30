c9302d8532855f4a8f1c018d9b43fb02176dd83d
martinristow <martinristov084@gmail.com>
2024-08-31 01:58:03 +0200

    Updated `main.py` to reflect new internet speed thresholds and Twitter credentials
    - Updated PROMISED_DOWN and PROMISED_UP values to 15 and 4 respectively.
    - Added `TWITTER_EMAIL`, `TWITTER_PASSWORD`, and `TWITTER_USERNAME` environment variables to match new Twitter login requirements.
    - Modified the instantiation of `InternetSpeedTwitterBot` with the new internet speed parameters.
    - Updated the call to `tweet_at_provider` to include Twitter login credentials and new speed thresholds.

--- projects/web-scraping/Selenium/internet-speed-twitter-complaint-bot/main.py
+++ projects/web-scraping/Selenium/internet-speed-twitter-complaint-bot/main.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PROMISED_DOWN = 150
-PROMISED_UP = 10
+PROMISED_DOWN = 15
+PROMISED_UP = 4
 INTERNET_SPEED_DRIVER_PATH = "https://www.speedtest.net/"
-# INTERNET_SPEED_DRIVER_PATH = "https://www.google.com/"
 TWITTER_DRIVER_PATH = "https://x.com/home"
 TWITTER_EMAIL = os.environ["TWITTER_EMAIL"]
 TWITTER_PASSWORD = os.environ["TWITTER_PASSWORD"]
@@ -16,4 +15,4 @@ TWITTER_USERNAME = os.environ["TWITTER_USERNAME"]
 internet = InternetSpeedTwitterBot(driver_path=INTERNET_SPEED_DRIVER_PATH)
 internet.get_internet_speed()
 
-twitter = internet.tweet_at_provider(TWITTER_DRIVER_PATH)
+twitter = internet.tweet_at_provider(TWITTER_DRIVER_PATH, TWITTER_EMAIL, TWITTER_USERNAME, TWITTER_PASSWORD, PROMISED_DOWN, PROMISED_UP)

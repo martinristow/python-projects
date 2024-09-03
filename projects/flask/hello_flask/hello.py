dd01a5c9533c0ca58739502186f45f6500b01f02
martinristow <martinristov084@gmail.com>
2024-09-03 17:41:19 +0200

    Enhance Flask app with HTML styling and decorators
    - Updated the root route to include centered heading, paragraph, and an embedded GIF.
    - Added custom decorators to style the `/bye` route with bold, emphasis, and underlined text.
    - Retained dynamic route `/username/<name>/<int:number>` for personalized greetings.

--- projects/flask/hello_flask/hello.py
+++ projects/flask/hello_flask/hello.py
@@ -5,18 +5,44 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ("<h1 style='text-align: center'>Hello, World!</h1>"
+            "<p>This text is a paragraph</p>"
+            "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNW15MWkwZnBrY28xN3g2ZHprcG5icGEzOWRibjU5bjNpaGJ5aTBtOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/K1tgb1IUeBOgw/giphy.gif' width=500px>")
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
+# Different routes using the app.route decorator
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return "Bye"
 
 
+# Creating variable paths and converting the path to a specified data type
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
     return f"Hello there {name}, you are {number} years old!"
 
 
 if __name__ == "__main__":
+    # Run the app in debug mode to auto-reload
     app.run(debug=True)

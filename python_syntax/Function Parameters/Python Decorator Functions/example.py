0fb42ffb620dd926b42e321f8c2f0432ce81177b
martinristow <martinristov084@gmail.com>
2024-09-03 17:52:30 +0200

    Add authentication decorator for blog post creation
    - Implemented `User` class to manage user details and login status.
    - Created `is_authenticated_decorator` to check if a user is logged in before allowing blog post creation.
    - Added `create_blog_post` function that prints a message if the user is authenticated.
    - Tested the decorator with a user instance and confirmed functionality.

---
+++ python_syntax/Function Parameters/Python Decorator Functions/example.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged = False
+
+
+def is_authenticated_decorator(function):
+    def wrapper(*args):
+        if args[0].is_logged:
+            function(args[0])
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("Martin")
+new_user.is_logged = True
+create_blog_post(new_user)

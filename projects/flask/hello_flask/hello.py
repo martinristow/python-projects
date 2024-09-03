3d673da671cd01cd3608c3fb94b351228a7f6087
martinristow <martinristov084@gmail.com>
2024-09-03 16:31:56 +0200

    Add dynamic routing and new routes to Flask app
    - Added a new `/bye` route that returns a "Bye" message.
    - Implemented a dynamic route `/username/<name>/<int:number>` that greets the user by name and age.
    - Enabled debug mode in Flask for easier development.

--- projects/flask/hello_flask/hello.py
+++ projects/flask/hello_flask/hello.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+@app.route("/bye")
+def bye():
+    return "Bye"
+
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old!"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

1e804e0ebbf52d81afa179e1420e9eaa99329aed
martinristow <martinristov084@gmail.com>
2024-09-05 16:42:40 +0200

    Enhance Routes and Data Handling in main.py
    - Added dynamic data handling for the 'post' route to fetch and display specific post details.
    - Integrated current date and year into templates for consistent footer and header information.
    - Included debugging print statement for post data retrieval in the 'post' route.

--- projects/bootstrap/upgraded-blog/main.py
+++ projects/bootstrap/upgraded-blog/main.py
@@ -1,28 +1,37 @@
 from flask import Flask, render_template
-
+import requests, datetime
+time_now = datetime.datetime.now().date()
+year = datetime.datetime.now().year
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/index")
 def index():
-    return render_template("index.html")
+    respond = requests.get(url="https://api.npoint.io/f5ff69b0c317d2aed364")
+    json_data = respond.json()
+    return render_template("index.html", all_posts=json_data, time_now=time_now, year=year)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", year=year)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=year)
 
-@app.route("/post")
-def post():
-    return render_template("post.html")
+@app.route("/post/<int:id>")
+def post(id):
+    respond = requests.get(url="https://api.npoint.io/f5ff69b0c317d2aed364")
+    json_data = respond.json()
+    post1 = None
+    for post in json_data:
+        if post["id"] == id:
+            post1 = post
+            break
+    print(post1)  # Debugging line to see the post data
+    return render_template("post.html", post=post1, time_now=time_now, year=year)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

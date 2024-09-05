2d6fe8dbddb420c00be6e1246f21260457765d04
martinristow <martinristov084@gmail.com>
2024-09-06 00:56:39 +0200

    Initial setup of Flask-WTF login form
    - Created basic Flask app with routes for home and login
    - Set up Flask-WTF form with email and password validation
    - Added secret key for form security
    
    Fixed login functionality and added login validation
    
    - Corrected login logic to check for matching email and password
    - Improved validation flow for form submissions
    - Ensured form redirects to the correct template based on validation results

---
+++ projects/flask/Flask-WTF/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+email = ""
+password = ""
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(message="Ne moze prazno da go ostaves poleto"), Email(message="Greska Email")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, message="Mora povekje od 8 karakteri")])
+    submit = SubmitField(label="Log In")
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+
+    login_form = MyForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == email and login_form.password.data:
+            return render_template("success.html")
+        return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

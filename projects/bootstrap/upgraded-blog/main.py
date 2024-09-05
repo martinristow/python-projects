340a2109c88ee40289bf2eb758e81e414c874872
martinristow <martinristov084@gmail.com>
2024-09-05 14:41:35 +0200

    Initial setup for upgraded-blog project
    - Added Flask routing in main.py with routes for home, index, about, contact, and post pages

---
+++ projects/bootstrap/upgraded-blog/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

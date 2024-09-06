3da847cfe795e4ae4a6847fc61f31105ce13bd51
martinristow <martinristov084@gmail.com>
2024-09-06 02:04:23 +0200

    Integrated Flask-Bootstrap into the project
    - Added Bootstrap5 for improved styling
    - Updated main.py to initialize Bootstrap
    - No changes in form functionality or routes

--- projects/flask/Flask-WTF/main.py
+++ projects/flask/Flask-WTF/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-
+from flask_bootstrap import Bootstrap5
 email = ""
 password = ""
 
@@ -13,6 +13,7 @@ class MyForm(FlaskForm):
     submit = SubmitField(label="Log In")
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 

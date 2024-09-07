e6a1b2024274a035c3f746609fb4cf61f05d0f3e
martinristow <martinristov084@gmail.com>
2024-09-07 14:08:23 +0200

    Refactored SQLite setup to use Flask SQLAlchemy
    - Switched from sqlite3 to Flask SQLAlchemy for database management.
    - Created new 'Book' model with id, title, author, and rating fields.
    - Configured the database URI for the new 'new-books-collection.db' file.
    - Added functionality to create a new book record using SQLAlchemy.

--- python_syntax/SQLite/main.py
+++ python_syntax/SQLite/main.py
@@ -1,10 +1,48 @@
-import sqlite3
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db") # pravime konekcija so bazata
+#
+# cursor = db.cursor() # kreirame kursor koj sto ke ni sluzi za da ja kontrolirame bazata
+#
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(2, 'Martin Ristov', 'Martin', '9.3')")
+# db.commit()
 
-db = sqlite3.connect("books-collection.db") # pravime konekcija so bazata
 
-cursor = db.cursor() # kreirame kursor koj sto ke ni sluzi za da ja kontrolirame bazata
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+##CREATE DATABASE
+class Base(DeclarativeBase):
+  pass
 
-cursor.execute("INSERT INTO books VALUES(2, 'Martin Ristov', 'Martin', '9.3')")
-db.commit()
+db = SQLAlchemy(model_class=Base)
+
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False) # nullable=False znaci deka nema da dozvoli NULL vrednosti
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+with app.app_context():
+    new_book = Book(id=2, title="Martin Ristov", author="J. K. Rowling", rating=9.9)
+    db.session.add(new_book)
+    db.session.commit()

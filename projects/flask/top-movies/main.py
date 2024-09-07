0b4a0c68e46106a8c0c805e351484c8815396936
martinristow <martinristov084@gmail.com>
2024-09-07 18:09:46 +0200

    Update main.py
    - Added new database schema for Movie model with fields: title, year, description, rating, ranking, review, and img_url.
    - Configured and initialized SQLAlchemy for SQLite database.
    - Removed commented-out code for adding initial movie data.

--- projects/flask/top-movies/main.py
+++ projects/flask/top-movies/main.py
@@ -13,15 +13,49 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 Bootstrap5(app)
 
+
 # CREATE DB
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-movies.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(nullable=False)
+    ranking: Mapped[str] = mapped_column(String(250), nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+# with app.app_context():
+#     for movie in movies:
+#         db.session.add(movie)
+#         db.session.commit()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # Read All Records
+    with app.app_context():
+        result = db.session.execute(db.select(Movie).order_by(Movie.title))
+        all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
+
 
 
 if __name__ == '__main__':

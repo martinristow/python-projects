bbadfcd6c6ff0ee1f3147e0e9d3acc60f7aea874
martinristow <martinristov084@gmail.com>
2024-09-09 18:57:59 +0200

    Implement RESTful API for cafe database
    - Set up Flask and SQLAlchemy to manage cafe records
    - Created Cafe model with columns for name, location, amenities, and coffee price
    - Added endpoints:
      - `/random`: Get a random cafe from the database
      - `/all`: Retrieve a list of all cafes
      - `/search`: Search for cafes by location
      - `/add`: Add a new cafe via a POST request
      - `/update-price/<cafe_id>`: Update the price of a cafe based on its ID
      - `/report-closed/<cafe_id>`: Delete a cafe using its ID and API key validation
    - Added error handling for invalid or missing data
    - Integrated JSON responses for API endpoints

---
+++ projects/flask-restful/cafe-api/main.py
@@ -0,0 +1,162 @@
+import random
+
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+
+
+app = Flask(__name__)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    # return jsonify(cafe={
+    #     # Omit the id from the response
+    #     # "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #
+    #     # Put some properties in a sub-category
+    #     "amenities": {
+    #         "seats": random_cafe.seats,
+    #         "has_toilet": random_cafe.has_toilet,
+    #         "has_wifi": random_cafe.has_wifi,
+    #         "has_sockets": random_cafe.has_sockets,
+    #         "can_take_calls": random_cafe.can_take_calls,
+    #         "coffee_price": random_cafe.coffee_price,
+    #     }
+    # })
+    # Simply convert the random_cafe data record to a dictionary of key-value pairs.
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafe = result.scalars().all()
+
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafe])
+
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
+    # Note, this may get more than one cafe per location
+    all_cafe = result.scalars().all()
+    if all_cafe:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafe])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+# HTTP GET - Read Record
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+
+
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
+# Updating the price of a cafe based on a particular id:
+# http://127.0.0.1:5000/update-price/CAFE_ID?new_price=£5.67
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.get_or_404(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        ## Just add the code after the jsonify method. 200 = Ok
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        #404 = Resource not found
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+
+
+# Deletes a cafe with a particular id. Change the request type to "Delete" in Postman
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.get_or_404(Cafe, cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+
+if __name__ == '__main__':
+    app.run(debug=True)

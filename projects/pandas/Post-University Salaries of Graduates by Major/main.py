34a91fa2593dad59b8e3f2c29334e6a741e108b2
martinristow <martinristov084@gmail.com>
2024-09-12 17:12:09 +0200

    Added analysis for highest and lowest salaries by college major
    Identified the college major with the highest mid-career salary and printed the corresponding salary and major.
    Determined the major with the lowest starting salary and printed the relevant details.
    Found the major with the lowest mid-career salary and printed the salary and major.
    Cleaned the DataFrame to remove rows with null values before analysis.

--- projects/pandas/Post-University Salaries of Graduates by Major/main.py
+++ projects/pandas/Post-University Salaries of Graduates by Major/main.py
@@ -39,3 +39,37 @@ print(df.tail())
 clean_df = df.dropna()
 print(clean_df.isna)
 print(clean_df.tail())
+
+# --------------------------------------------------------------
+
+# What college major has the highest mid-career salary? How much do graduates with this major earn?
+# (Mid-career is defined as having 10+ years of experience).
+clean_df['Mid-Career Median Salary'].idxmax()
+print(clean_df['Mid-Career Median Salary'].loc[8])
+print(clean_df['Undergraduate Major'][8])
+
+# print(clean_df['Mid-Career Median Salary'].max())
+# print(f"Index for the max mid career salary: {clean_df['Mid-Career Median Salary'].idxmax()}")
+# clean_df['Undergraduate Major'][8]
+
+# --------------------------------------------------------------
+
+# Which college major has the lowest starting salary and how much do graduates earn after university?
+# The Lowest Starting and Mid-Career Salary
+clean_df['Starting Median Salary'].idxmin()
+print(clean_df['Starting Median Salary'][49])
+print(clean_df['Undergraduate Major'][49])
+
+
+# The Lowest Starting and Mid-Career Salary
+# print(clean_df['Starting Median Salary'].min())
+# clean_df['Undergraduate Major'].loc[clean_df['Starting Median Salary'].idxmin()]
+
+# --------------------------------------------------------------
+
+# Which college major has the lowest mid-career salary and how much can people expect to earn with this degree?
+clean_df['Mid-Career Median Salary'].idxmin()
+print(clean_df['Mid-Career Median Salary'][18])
+print(clean_df['Undergraduate Major'][18])
+
+# clean_df.loc[clean_df['Mid-Career Median Salary'].idxmin()]

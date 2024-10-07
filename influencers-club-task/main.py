810488fbefd09dd14a217caf35071790770c1e1c
martinristow <martinristov084@gmail.com>
2024-10-07 22:26:34 +0200

    Moved

---
+++ influencers-club-task/main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = ('https://www.newegg.com/Clearance-Store/EventSaleStore/ID-697?cm_sp=Head_Navigation-_-Unde'
+       'r_Search_Bar-_-Clearance+Deals&icid=755812')
+
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
+
+response = requests.get(url=URL, headers=header)
+
+print(response.headers['Content-Type'])
+print(response.text)
+

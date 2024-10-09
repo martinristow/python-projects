36476d21073bf3ba9300cc44d805c947a4a3ac00
martinristow <martinristov084@gmail.com>
2024-10-09 22:10:38 +0200

    Working with Sessions
    - Set Session
    - Get Data from Session
    - Clear Session

--- projects/flask-new/app.py
+++ projects/flask-new/app.py
@@ -1,83 +1,37 @@
-from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory, jsonify
+from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory, jsonify, session
 import pandas as pd
 import os
 import uuid
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.secret_key = 'SOME KEY'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if username == 'martin' and password == 'password':
-            return 'Success'
-        else:
-            return 'Failure'
+    return render_template('index.html', message='Index')
 
 
-@app.route('/file_upload', methods=['POST'])
-def file_upload():
-    file = request.files['file']
-
-    if file.content_type == 'text/plain':
-        return file.read().decode()
-    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
-        df = pd.read_excel(file)
-        return df.to_html()
+@app.route('/set_data')
+def set_data():
+    session['name'] = 'Mike'
+    session['other'] = 'Hello World'
+    return render_template('index.html', message='Session data set.')
 
 
-@app.route('/convert_csv', methods=['POST'])
-def convert_csv():
-    file = request.files['file']
-
-    df = pd.read_excel(file)
-    response = Response(
-        df.to_csv(),
-        mimetype='text/csv',
-        headers={
-            'Content-Disposition': 'attachment; filename=result.csv'
-        }
-    )
-
-    return response
+@app.route('/get_data')
+def get_data():
+    if 'name' in session.keys() and 'other' in session.keys():
+        name = session['name']
+        other = session['other']
+        return render_template('index.html', message=f'Name:{name}, other:{other}')
+    else:
+        return render_template('index.html', message='No session found')
 
 
-
-@app.route('/convert_csv_two', methods=['POST'])
-def convert_csv_two():
-    file = request.files['file']
-
-    df = pd.read_excel(file)
-
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
-
-    filename = f'{uuid.uuid4()}.csv'
-    df.to_csv(os.path.join('downloads', filename))
-
-    return render_template('download.html', filename=filename)
-
-
-@app.route('/download/<filename>')
-def download(filename):
-    return send_from_directory('downloads', filename, download_name='result.csv')
-
-
-
-@app.route('/handle_post', methods=['POST'])
-def handle_post():
-    greeting = request.json['greeting']
-    name = request.json['name']
-
-    with open('file.txt', 'w') as f:
-        f.write(f'{greeting}, {name}')
-
-    return jsonify({'message': 'Successfully written'})
-
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return render_template('index.html', message='Session cleared.')
 
 if __name__ == "__main__":
     app.run(debug=True)

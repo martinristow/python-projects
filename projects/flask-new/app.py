717eba11f259e7f843b0bdf0af7f962b3a13466c
martinristow <martinristov084@gmail.com>
2024-10-09 22:32:52 +0200

    Add a few new things in code:
    - Set Cookie
    - Get Cookie
    - Remove Cookie
    - Login & using Flash

--- projects/flask-new/app.py
+++ projects/flask-new/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory, jsonify, session
+from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory, jsonify, session, make_response, flash
 import pandas as pd
 import os
 import uuid
@@ -32,6 +32,43 @@ def get_data():
 def clear_session():
     session.clear()
     return render_template('index.html', message='Session cleared.')
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response(render_template('index.html', message='Cookie set.'))
+    response.set_cookie('cookie_name', 'cookie_value')
+    return response
+
+
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies['cookie_name']
+    return render_template('index.html', message=f'Cookie Value:{cookie_value}')
+
+
+@app.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template('index.html', message='Cookie remove.'))
+    response.set_cookie('cookie_name', expires=0)
+    return response
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/login.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'martin' and password == '1234':
+            flash('Successful Login')
+            return render_template('index.html', message='')
+        else:
+            flash('Login Failed')
+            return render_template('index.html', message='')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

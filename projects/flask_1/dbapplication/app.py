97569bd9694e5f699df1f294539857a8f7df6a62
martinristow <martinristov084@gmail.com>
2024-10-10 19:17:21 +0200

    Import DataBase with Flask

---
+++ projects/flask_1/dbapplication/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+
+    db.init_app(app)
+
+    from routes import register_routes
+    register_routes(app, db)
+
+    migrate = Migrate(app, db)
+
+    return app

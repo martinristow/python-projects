80914c143a9a37056173f8ecc49d8807f773545d
martinristow <martinristov084@gmail.com>
2024-10-13 00:26:43 +0200

    User Table Design
    - id
    - username
    - email
    - password

---
+++ projects/univerzal-project-flask/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)

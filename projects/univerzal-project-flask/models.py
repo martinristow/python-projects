0ffede8b56fa256ece4c35aad91bf88fc8e676c1
martinristow <martinristov084@gmail.com>
2024-10-13 20:36:21 +0200

    Add new tables

--- projects/univerzal-project-flask/models.py
+++ projects/univerzal-project-flask/models.py
@@ -8,3 +8,19 @@ class User(db.Model):
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    activity_name = db.Column(db.String(50), nullable=False)
+    cultivated_agricultural_area = db.Column(db.String(50), nullable=False)
+    field_name = db.Column(db.String(50), nullable=False)
+    field_area = db.Column(db.String(50), nullable=False)
+
+
+class FieldYield(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    field_name = db.Column(db.String(50), nullable=False)
+    agricultural_culture = db.Column(db.String(50), nullable=False)
+    field_area = db.Column(db.String(50), nullable=False)
+    field_yield = db.Column(db.String(50), nullable=False)
